cf0499640e6e8cf778a81e1489ecd9d0563b96c8
stuartj073 <stuartj073@gmail.com>
2021-05-18 09:38:49 +0000

    updated nav bar to include burger menu, rearranged quotes section

---
+++ app.js
@@ -0,0 +1,16 @@
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+openMenu.addEventListener('click',open);
+close.Menu.addEventListener('click',close)
+
+function show(){
+    mainMenu.Menu.style.display='flex';
+    mainMenu.style.top='0';
+}
+
+function close() {
+    mainMenu.Menu.style.top ='-100%';
+    
+}
